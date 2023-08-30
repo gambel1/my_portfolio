d461d6ab109ee0ee8f74dc031bdb8d56dc861f27
Алексей <electrifyx@gmail.com>
2023-08-30 16:06:58 +0300

    added cv

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
-import css from '../Home/Home.module.css'
+import css from "../Home/Home.module.css";
 import sprite from "../../images/sprite.svg";
+import cv from '../../cv.pdf'
 import { Link } from "react-router-dom";
 
 export default function Home() {
@@ -7,29 +8,49 @@ export default function Home() {
     <>
       {/* <div className="container"> */}
       <div className={css.home}>
+        <div>
+          <picture></picture>
+        </div>
         <h1 className={css.homeTitle}>Alexey Potapenko</h1>
         <p className={css.homeSubtitle}>I am a Junior Full-Stack developer</p>
         <ul className={css.homeList}>
           <li>
-            <Link className={css.homeLink}>
-              <svg className={css.homeImageCurrent} width="25" height="25">
+            <a
+              href="https://github.com/gambel1"
+              target="_blank"
+              rel="noreferrer noopener"
+              className={css.homeLink}
+            >
+              <svg width="25" height="25">
                 <use href={sprite + "#icon-github"}></use>
               </svg>
-            </Link>
+            </a>
           </li>
           <li>
-            <Link className={css.homeLink}>
-              <svg className={css.homeImage} width="30" height="30">
+            <a
+              href="https://www.linkedin.com/in/alexpotapenko/"
+              target="_blank"
+              rel="noreferrer noopener"
+              className={css.homeLink}
+            >
+              <svg width="25" height="25">
                 <use href={sprite + "#icon-linkedin"}></use>
               </svg>
-            </Link>
+            </a>
           </li>
           <li>
             <Link className={css.homeLink}>
-              <svg className={css.homeImage} width="30" height="30">
+              <svg width="25" height="25">
                 <use href={sprite + "#icon-telegram"}></use>
               </svg>
             </Link>
+          </li>
+          <li>
+            <a href={cv} download title="download_cv" className={css.homeLink}>
+              <svg width="25" height="25">
+                <use href={sprite + "#icon-cv"}></use>
+              </svg>
+            </a>
           </li>
         </ul>
       </div>
