19d46e00af9d4b01c2ca97befd3c4815bee1b034
Алексей <electrifyx@gmail.com>
2023-09-14 15:00:03 +0300

    add slider

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,53 +1,70 @@
-import photo450 from "../../images/photo450.jpg";
-import technology450 from "../../images/technology450.jpg";
-import backgraund from "../../images/backgraundLinkedin.png";
-import myPhoto from "../../images/myPhoto.jpg";
-import { Swiper, SwiperSlide } from "swiper/react";
 import css from "../Portfolio/Portfolio.module.css";
+import imageFinder from '../../images/imageFinder.jpg'
+import mimino from '../../images/mimino.jpg'
+import movies from '../../images/movies.jpg'
+import news from '../../images/news.jpg'
+import phonebook from '../../images/phonebook.jpg'
+import taskPro from '../../images/taskPro.jpg'
+import test from '../../images/test.jpg'
+import webstudio from '../../images/webstudio.jpg'
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 export default function Portfolio() {
   return (
-    <Swiper
-      className={css.swiper}
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {/* <SwiperSlide>
-        <a href={photo450}>Slide 1</a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href={technology450}>Slide 2</a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href={backgraund}>Slide 3</a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href={myPhoto}>Slide 4</a>
-      </SwiperSlide> */}
-      <SwiperSlide>
-        <a href="https://gambel1.github.io/goit-react-hw-08-phonebook/">
-          <img src={photo450} alt='one'/>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href="https://gambel1.github.io/goit-react-hw-02-feedback/">
-          <img src={technology450} alt='two'/>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href="https://gambel1.github.io/goit-react-hw-03-image-finder/">
-          <img src={backgraund} alt='three'/>
-        </a>
-      </SwiperSlide>
-      <SwiperSlide>
-        <a href="https://gambel1.github.io/goit-react-hw-03-image-finder/">
-          <img src={myPhoto} alt='hour'/>
-        </a>
-      </SwiperSlide>
-      ...
-    </Swiper>
+    <section className={css.portfolio}>
+      <div className="container">
+        <h1 className={css.portfolioTitle}>MY WORKS</h1>
+        <Swiper
+          className={css.swiper}
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/goit-markup-hw-08/index.html">
+              <img src={webstudio} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/mimino-project-13/">
+              <img src={mimino} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/news-site/">
+              <img src={news} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/goit-react-hw-04-images/">
+              <img src={imageFinder} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/goit-react-hw-05-movies/">
+              <img src={movies} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/goit-react-hw-08-phonebook/">
+              <img src={phonebook} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/testGoIT/">
+              <img src={test} alt="" />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="https://gambel1.github.io/taskProjectFront/">
+              <img src={taskPro} alt="" />
+            </a>
+          </SwiperSlide>
+          ...
+        </Swiper>
+      </div>
+    </section>
   );
 }
