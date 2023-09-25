acf29e9e07b7bc81dce91cf9d3200524c3a98829
Алексей <electrifyx@gmail.com>
2023-09-25 11:16:51 +0300

    fix modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import Header from './Header/Header';
 import Home from './Home/Home';
 import Portfolio from './Portfolio/Portfolio';
 import Footer from './Footer/Footer';
-import { useRef, useState } from 'react';
+import { useRef} from 'react';
 
 export default function App() {
     const ref = useRef(null);
